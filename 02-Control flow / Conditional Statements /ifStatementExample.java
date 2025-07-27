1ba142fe4d7303466eb363bd79efb833064c9b4c
Naina Kar <nainakar2000@gmail.com>
2025-07-27 09:44:04 +0530

    Create ifStatementExample.java

---
+++ 02-Control flow / Conditional Statements /ifStatementExample.java
@@ -0,0 +1,12 @@
+public class IfStatementExample {
+    public static void main(String[] args) {
+        int number = 10;
+
+        // if the number is positive
+        if (number > 0) {
+            System.out.println("The number is positive.");
+        }
+
+        System.out.println("This statement is always executed.");
+    }
+}
